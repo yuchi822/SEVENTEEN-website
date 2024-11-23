4cda2d20e469a6583d3e1e3531262cff4b9d7dbd
FUKUMA1210 <ecilanfrost1210@gmail.com>
2024-11-23 14:36:51 +0800

    Social

--- src/SocialMedia.tsx
+++ src/SocialMedia.tsx
@@ -4,19 +4,19 @@ import './assets/Style.css';
 const SocialMedia: React.FC = () => {
   return (
     <footer>
-      <a href="https://www.facebook.com/seventeen" target="_blank" rel="noreferrer">
+      <a href="https://www.facebook.com/seventeennews" target="_blank" rel="noreferrer">
         Facebook
       </a>
       <a href="https://twitter.com/pledis_17" target="_blank" rel="noreferrer">
         X
       </a>
-      <a href="https://www.instagram.com/svt" target="_blank" rel="noreferrer">
+      <a href="https://www.instagram.com/saythename_17/" target="_blank" rel="noreferrer">
         Instagram
       </a>
-      <a href="https://www.youtube.com/c/SEVENTEEN" target="_blank" rel="noreferrer">
+      <a href="https://www.youtube.com/@pledis17" target="_blank" rel="noreferrer">
         YouTube
       </a>
-      <a href="https://www.tiktok.com/@seventeen" target="_blank" rel="noreferrer">
+      <a href="https://www.tiktok.com/@seventeen17_official?is_from_webapp=1&sender_device=pc" target="_blank" rel="noreferrer">
         TikTok
       </a>
       <a href="https://weverse.io/seventeen" target="_blank" rel="noreferrer">
